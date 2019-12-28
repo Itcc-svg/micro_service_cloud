99328ee3d646b980210d2d7a4518b2acafad15ce
cml123 <3844063232@qq.com>
2019-12-28 17:17:14 +0800

    add Zuul module

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/itcc/springcloud/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.itcc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 注解表明 springboot EnableEurekaServer
+ * Eureka 通过localhost:7001检测
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
